ce15f55b4e721d0801eaf8e7e5913c16a3020187
Lisa Lenhart <Lenhart.Lisa.K@gmail.com>
2022-09-02 14:45:06 -0700

    Updated menu, disabled debug, added formating
    Change-Id: I08cded973a3d5e554ff0a0052c05ec70adba9eea

--- SwitchCase/src/SwitchCase.java
+++ SwitchCase/src/SwitchCase.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class SwitchCase {
 	
 	static boolean again = true;
-	static boolean dgb = true;
+	static boolean dgb = false;
 	
 	public static void main(String[] args) {
 		if(dgb) {System.out.println("debug is rockin");}
@@ -14,6 +14,12 @@ class SwitchCase {
 			
 			Scanner Scanner = new Scanner(System.in);
 			System.out.println("Welcome to Bank of Lisa & Jason, choose an option from 1-3: ");
+			System.out.println("");
+			
+			System.out.println("Option 1: Deposit into your account.");
+			System.out.println("Option 2: Account details.");
+			System.out.println("Option 3: Withdrawl");
+
 			
 			int userInput = Scanner.nextInt();
 			System.out.println();
@@ -26,16 +32,18 @@ class SwitchCase {
 				break;
 				
 			case 2:				// displaying details of account: display()
-				System.out.println("Display operation");
+				System.out.println("Account details successfully shown.");
 				break;
 				
 			case 3: 			// deposit operation: deposit()
-				System.out.println("Deposit operation");
+				System.out.println("Withdrawl was accepted.");
 				break;
 				
 			default:			// If no case matches, default will be executed
 				System.out.println("Invalid choice: (Sorry, enter 1-3)");
-			}	
+			}
+			
+			System.out.println("");
 			System.out.println("Would you like to choose again? 1 = Y or 2 = N:  "); 
 			userInput = Scanner.nextInt();
 			if(userInput == 1) {
@@ -43,6 +51,7 @@ class SwitchCase {
 			}
 			else {
 				again = false;
+				System.out.println("");
 				System.out.println("Thank for Banking with the Bank of Lisa and Jason");
 			}			
 		}    //end-while
